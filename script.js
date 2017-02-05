a544a7ba0b20f211ad035ce9e44d770fdeae1a51
Liz Bachman <eleighbachman@gmail.com>
2017-02-04 20:52:09 -0700

    finished project

---
+++ script.js
@@ -0,0 +1,27 @@
+// user selects a button
+// button id "1" (rock), "2" (paper), "3" (scissors) is conveyed
+// computer randomly chooses a number between 1 - 3
+// compare the output and give result
+
+/* Scissors vs. Paper - scissors wins
+  Scissors vs. Rock - rock wins
+  Scissors vs. Scissors - retry
+  Paper vs. Rock - Paper wins
+  Paper vs. Paper - retry
+  Rock vs. Rock - retry
+
+
+  If userVal === compVal : retry!
+
+*/
+
+/* CASES
+
+1 - 2
+1 - 3
+2 - 1
+2 - 3
+3 - 1
+3 - 3
+
+*/
